c2e3b05c870e2b5ed78449d598f91f84987b0a6e
baggerest <baggerest@yahoo.com.tw>
2017-07-10 15:23:40 +0800

    https://github.com/baggerest/Caps_Lock_listen

---
+++ Caps_Lock_listen/Form1.cs
@@ -0,0 +1,273 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Caps_Lock_listen
+{
+    public partial class Form1 : Form
+    {
+        private const uint WS_EX_LAYERED = 0x80000;
+        private const int WS_EX_TRANSPARENT = 0x20;
+        private const int GWL_STYLE = (-16);
+        private const int GWL_EXSTYLE = (-20);
+        private const int LWA_ALPHA = 0x2;
+
+        [DllImport("user32", EntryPoint = "SetWindowLong")]
+        private static extern uint SetWindowLong(
+        IntPtr hwnd,
+        int nIndex,
+        uint dwNewLong
+        );
+
+        [DllImport("user32", EntryPoint = "GetWindowLong")]
+        private static extern uint GetWindowLong(
+        IntPtr hwnd,
+        int nIndex
+        );
+
+        [DllImport("user32", EntryPoint = "SetLayeredWindowAttributes")]
+        private static extern int SetLayeredWindowAttributes(
+        IntPtr hwnd,
+        int crKey,
+        int bAlpha,
+        int dwFlags
+        );
+
+        /// <summary>
+        /// 使窗口有鼠标穿透功能
+        /// </summary>
+        public void CanPenetrate()
+        {
+            uint intExTemp = GetWindowLong(this.Handle, GWL_EXSTYLE);
+            uint oldGWLEx = SetWindowLong(this.Handle, GWL_EXSTYLE, WS_EX_TRANSPARENT | WS_EX_LAYERED);
+            SetLayeredWindowAttributes(this.Handle, 0, 100, LWA_ALPHA);
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Color a = Form1.DefaultBackColor;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            int screenWidth = Screen.PrimaryScreen.Bounds.Width;
+            int screenHeight = Screen.PrimaryScreen.Bounds.Height;
+            this.Top = 0;
+            this.Left = 0;
+            this.Width = screenWidth;
+            this.Height = 10;
+            label1.Left = screenWidth / 2;
+            CanPenetrate();
+            this.BackColor = Console.CapsLock ? Color.Red : a;
+            var kh = new KeyboardHook(true);
+            kh.KeyDown += Kh_KeyDown;
+        }
+
+        private void Kh_KeyDown(Keys key, bool Shift, bool Ctrl, bool Alt)
+        {
+            if (key == Keys.Capital)
+            {
+                this.BackColor = !Console.CapsLock ? Color.Red : a;
+                label1.Text = !Console.CapsLock ? "Caps Lock":"";
+            }
+        }
+    }
+
+    public class KeyboardHook : IDisposable
+    {
+        bool Global = true;
+
+        public delegate void LocalKeyEventHandler(Keys key, bool Shift, bool Ctrl, bool Alt);
+        public event LocalKeyEventHandler KeyDown;
+        public event LocalKeyEventHandler KeyUp;
+
+        public delegate int CallbackDelegate(int Code, int W, int L);
+
+        [StructLayout(LayoutKind.Sequential, CharSet = CharSet.Ansi)]
+        public struct KBDLLHookStruct
+        {
+            public Int32 vkCode;
+            public Int32 scanCode;
+            public Int32 flags;
+            public Int32 time;
+            public Int32 dwExtraInfo;
+        }
+
+        [DllImport("user32", CallingConvention = CallingConvention.StdCall)]
+        private static extern int SetWindowsHookEx(HookType idHook, CallbackDelegate lpfn, int hInstance, int threadId);
+
+        [DllImport("user32", CallingConvention = CallingConvention.StdCall)]
+        private static extern bool UnhookWindowsHookEx(int idHook);
+
+        [DllImport("user32", CallingConvention = CallingConvention.StdCall)]
+        private static extern int CallNextHookEx(int idHook, int nCode, int wParam, int lParam);
+
+        [DllImport("kernel32.dll", CallingConvention = CallingConvention.StdCall)]
+        private static extern int GetCurrentThreadId();
+
+        public enum HookType : int
+        {
+            WH_JOURNALRECORD = 0,
+            WH_JOURNALPLAYBACK = 1,
+            WH_KEYBOARD = 2,
+            WH_GETMESSAGE = 3,
+            WH_CALLWNDPROC = 4,
+            WH_CBT = 5,
+            WH_SYSMSGFILTER = 6,
+            WH_MOUSE = 7,
+            WH_HARDWARE = 8,
+            WH_DEBUG = 9,
+            WH_SHELL = 10,
+            WH_FOREGROUNDIDLE = 11,
+            WH_CALLWNDPROCRET = 12,
+            WH_KEYBOARD_LL = 13,
+            WH_MOUSE_LL = 14
+        }
+
+        private int HookID = 0;
+        CallbackDelegate TheHookCB = null;
+
+        //Start hook
+        public KeyboardHook(bool Global)
+        {
+            this.Global = Global;
+            TheHookCB = new CallbackDelegate(KeybHookProc);
+            if (Global)
+            {
+                HookID = SetWindowsHookEx(HookType.WH_KEYBOARD_LL, TheHookCB,
+                    0, //0 for local hook. eller hwnd til user32 for global
+                    0); //0 for global hook. eller thread for hooken
+            }
+            else
+            {
+                HookID = SetWindowsHookEx(HookType.WH_KEYBOARD, TheHookCB,
+                    0, //0 for local hook. or hwnd to user32 for global
+                    GetCurrentThreadId()); //0 for global hook. or thread for the hook
+            }
+        }
+
+        bool IsFinalized = false;
+        ~KeyboardHook()
+        {
+            if (!IsFinalized)
+            {
+                UnhookWindowsHookEx(HookID);
+                IsFinalized = true;
+            }
+        }
+        public void Dispose()
+        {
+            if (!IsFinalized)
+            {
+                UnhookWindowsHookEx(HookID);
+                IsFinalized = true;
+            }
+        }
+
+        //The listener that will trigger events
+        private int KeybHookProc(int Code, int W, int L)
+        {
+            KBDLLHookStruct LS = new KBDLLHookStruct();
+            if (Code < 0)
+            {
+                return CallNextHookEx(HookID, Code, W, L);
+            }
+            try
+            {
+                if (!Global)
+                {
+                    if (Code == 3)
+                    {
+                        IntPtr ptr = IntPtr.Zero;
+
+                        int keydownup = L >> 30;
+                        if (keydownup == 0)
+                        {
+                            if (KeyDown != null) KeyDown((Keys)W, GetShiftPressed(), GetCtrlPressed(), GetAltPressed());
+                        }
+                        if (keydownup == -1)
+                        {
+                            if (KeyUp != null) KeyUp((Keys)W, GetShiftPressed(), GetCtrlPressed(), GetAltPressed());
+                        }
+                        //System.Diagnostics.Debug.WriteLine("Down: " + (Keys)W);
+                    }
+                }
+                else
+                {
+                    KeyEvents kEvent = (KeyEvents)W;
+
+                    Int32 vkCode = Marshal.ReadInt32((IntPtr)L); //Leser vkCode som er de første 32 bits hvor L peker.
+
+                    if (kEvent != KeyEvents.KeyDown && kEvent != KeyEvents.KeyUp && kEvent != KeyEvents.SKeyDown && kEvent != KeyEvents.SKeyUp)
+                    {
+                    }
+                    if (kEvent == KeyEvents.KeyDown || kEvent == KeyEvents.SKeyDown)
+                    {
+                        if (KeyDown != null) KeyDown((Keys)vkCode, GetShiftPressed(), GetCtrlPressed(), GetAltPressed());
+                    }
+                    if (kEvent == KeyEvents.KeyUp || kEvent == KeyEvents.SKeyUp)
+                    {
+                        if (KeyUp != null) KeyUp((Keys)vkCode, GetShiftPressed(), GetCtrlPressed(), GetAltPressed());
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                //Ignore all errors...
+            }
+
+            return CallNextHookEx(HookID, Code, W, L);
+
+        }
+
+        public enum KeyEvents
+        {
+            KeyDown = 0x0100,
+            KeyUp = 0x0101,
+            SKeyDown = 0x0104,
+            SKeyUp = 0x0105
+        }
+
+        [DllImport("user32.dll")]
+        static public extern short GetKeyState(System.Windows.Forms.Keys nVirtKey);
+
+        public static bool GetCapslock()
+        {
+            return Convert.ToBoolean(GetKeyState(System.Windows.Forms.Keys.CapsLock)) & true;
+        }
+        public static bool GetNumlock()
+        {
+            return Convert.ToBoolean(GetKeyState(System.Windows.Forms.Keys.NumLock)) & true;
+        }
+        public static bool GetScrollLock()
+        {
+            return Convert.ToBoolean(GetKeyState(System.Windows.Forms.Keys.Scroll)) & true;
+        }
+        public static bool GetShiftPressed()
+        {
+            int state = GetKeyState(System.Windows.Forms.Keys.ShiftKey);
+            if (state > 1 || state < -1) return true;
+            return false;
+        }
+        public static bool GetCtrlPressed()
+        {
+            int state = GetKeyState(System.Windows.Forms.Keys.ControlKey);
+            if (state > 1 || state < -1) return true;
+            return false;
+        }
+        public static bool GetAltPressed()
+        {
+            int state = GetKeyState(System.Windows.Forms.Keys.Menu);
+            if (state > 1 || state < -1) return true;
+            return false;
+        }
+    }
+}
